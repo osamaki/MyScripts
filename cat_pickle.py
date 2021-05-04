2b378fa830df0e739c0bceb90607e2126d2a5c5c
osamaki <ozaki@nlp.ist.i.kyoto-u.ac.jp>
2021-05-05 06:35:39 +0900

    change to use pprint

--- cat_pickle.py
+++ cat_pickle.py
@@ -2,9 +2,10 @@
 
 import sys
 import pickle
+import pprint
 
 args = sys.argv
 
 for file_name in args[1:]:
     with open(file_name, "rb")  as fin:
-        print(pickle.load(fin))
+        pprint.pprint(pickle.load(fin))
